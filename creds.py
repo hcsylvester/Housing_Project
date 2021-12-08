8d0cf93b8708581632dfc7f85fda692851eab684
Hunter Sylvester <huntersylvester@Hunters-MBP-4.MG8702>
2021-12-08 16:10:51 -0600

    Initial commit

---
+++ creds.py
@@ -0,0 +1,6 @@
+# This is sql database information needed to connect successfully
+
+username = 'root'
+password = 'Blue0rangetr33!'
+hostname = 'localhost'
+database = 'housing_project'
---
+++ files.py
@@ -0,0 +1,6 @@
+# Hunter C. Sylvester
+# Purpose: Create a file so that I may import this file to gain access to all 3 csv files
+
+housingFile = "/Users/huntersylvester/Desktop/HousingProject-master/housing-info.csv"
+incomeFile = "/Users/huntersylvester/Desktop/HousingProject-master/income-info.csv"
+zipFile = "/Users/huntersylvester/Desktop/HousingProject-master/zip-city-county-state.csv"
